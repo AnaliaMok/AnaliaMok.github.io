4aae46dca426a0e1c6ed106ed79e5a86dbd58be0
Analia Mok <axm2580@g.rit.edu>
2017-07-26 17:02:14 -0400

    Added all development files to production

---
+++ assets/includes/contact_page.php
@@ -0,0 +1,57 @@
+<div class="content-block" id="contact-page">
+    <!-- TODO: Add Content Form -->
+    <div class="left-side">
+        <h2>CONTACT ME</h2>
+        <!-- Email Sent to Main Email -->
+        <form action="https://www.enformed.io/xhp57kb1" method="post">
+            <div class="form-row">
+                <label>NAME</label>
+                <input type="text" name="name" required />
+            </div>
+
+            <div class="form-row">
+                <label>YOUR EMAIL ADDRESS</label>
+                <input type="email" name="email_address" required />
+            </div>
+
+            <div class="form-row">
+                <label>SUBJECT</label>
+                <input type="text" name="subject" required />
+            </div>
+
+            <div class="form-row">
+                <label>MESSAGE</label>
+                <textarea name="message" required></textarea>
+            </div>
+
+            <!-- Hidden Security Field -->
+            <input type="hidden" name="*honeypot" />
+            <!-- Changed Form Redirection to This Page -->
+            <!-- TODO: Change Value to .html -->
+            <!-- <input type="hidden" name="*redirect" value="index.html" /> -->
+
+            <!-- Reset and Submit Buttons -->
+            <div class="form-row">
+                <input type="text" id="empty-cell">
+                <div>
+                    <input type="reset" name="reset" value="CLEAR" class="blue-button">
+                    <input type="submit" name="submit" value="SEND" class="blue-button">
+                </div>
+            </div>
+
+        </form>
+    </div>
+    <div class="right-side">
+        <h2>OTHER MEDIA</h2>
+        <ul>
+            <li>LinkedIn: <a href="https://www.linkedin.com/in/AnaliaMok">linkedin.com/in/AnaliaMok</a></li>
+            <li>GitHub: <a href="https://analiamok.github.io/">analiamok.github.io</a></li>
+            <li>RIT JobZone: <a href="https://rit-csm.symplicity.com/profiles/analia.mok">rit-csm.symplicity.com/profiles/analia.mok</a></li>
+        </ul>
+        <p style="font-style: italic;">
+            **Please note that I am unavailable for hire until Spring 2019.
+            I am already committed for an internship for the Spring and Summer terms
+            of 2018.
+        </p>
+    </div>
+</div>
---
+++ assets/includes/projects.php
@@ -0,0 +1,251 @@
+<div class="projects-container" id="projects">
+    <!-- Left Arrow Button -->
+    <!-- <span class="arrows ion ion-android-arrow-dropleft-circle" id="left_btn"></span> -->
+
+    <div class="projects-container-inner">
+
+        <!-- Grader App -->
+        <div class="project-block">
+
+            <div class="project-content">
+                <!-- Project Title -->
+                <h2>Grader App</h2>
+                <!-- Project Role -->
+                <p class="proj-role"><em>(In Progress)</em> Personal Project - Solo</p>
+
+                <!-- Project Description -->
+                <p>
+                    Developing a grading web application to assist in the scheduling,
+                    and recording of student grades. This application will also
+                    generate text documents and spreadsheets of student grades.
+                    This application is aimed towards student graders looking
+                    for a more efficient way to grade and to produce documents
+                    for their assigned professor.
+                </p>
+
+                <!-- Related Tools -->
+                <h3>Tools</h3>
+                <ul>
+                    <li>HTML 5 // CSS 3 // jQuery 3.2.1</li>
+                    <li>PHP 5.6.3 // MySQL</li>
+                    <li>CodeIgniter 3.1.5</li>
+                    <li>Version Control: GitHub</li>
+                </ul>
+                <br>
+                <ul class="proj-links">
+                    <li>
+                        <span class="ion ion-social-github-outline"></span>
+                        <a href="https://github.com/AnaliaMok/GraderApplication">Code Viewable On GitHub</a>
+                    </li>
+                    <!-- Link to ReadMe File found on GitHub -->
+                    <li>
+                        <span class="ion ion-ios-paper-outline"></span>
+                        <a href="https://github.com/AnaliaMok/GraderApplication/blob/master/README.md">View ReadMe File</a>
+                    </li>
+                </ul>
+            </div>
+
+            <!-- <div class="button-group">
+                <div>
+                    <span class="ion ion-ios-email-outline"></span> -->
+                    <!-- Opens a README File -->
+                    <!-- <a href="#" class="ion ion-ios-paper-outline"></a>
+                </div>
+
+            </div> -->
+
+        </div><!-- End of Project Block -->
+
+        <!-- JavaScript Terminal Emulator -->
+        <div class="project-block">
+
+            <div class="project-content">
+                <!-- Project Title -->
+                <h2>JS Terminal Emulator</h2>
+                <!-- Project Role -->
+                <p class="proj-role">Solo Developer</p>
+
+                <!-- Project Description -->
+                <p>
+                    Developed a pseudo-Unix terminal - using only traditional
+                    front-end technologies. Implements the majority of basic
+                    Unix commands and options with a preset file structure. Meant
+                    to be used in a tutorial setting - not for production.
+                </p>
+
+                <!-- Related Tools -->
+                <h3>Tools</h3>
+                <ul>
+                    <li>HTML 5 // CSS 3 // JavaScript</li>
+                    <li>Version Control: Git</li>
+                </ul>
+                <br>
+                <ul class="proj-links">
+                    <li>
+                        <span class="ion ion-social-github-outline"></span>
+                        <a href="https://github.com/AnaliaMok/JSTerminalEmulator">Code Viewable On GitHub</a>
+                    </li>
+                    <!-- Link to ReadMe File found on GitHub -->
+                    <li>
+                        <span class="ion ion-ios-paper-outline"></span>
+                        <a href="https://github.com/AnaliaMok/JSTerminalEmulator/blob/master/README.md">View ReadMe File</a>
+                    </li>
+                </ul>
+            </div>
+
+            <!-- <div class="button-group">
+                <div>
+                    <span class="ion ion-ios-email-outline"></span> -->
+                    <!-- Opens a README File -->
+                    <!-- <a href="#" class="ion ion-ios-paper-outline"></a>
+                </div>
+
+            </div> -->
+
+        </div><!-- End of Project Block -->
+
+        <!-- Health Net -->
+        <div class="project-block">
+
+            <div class="project-content">
+                <!-- Project Title -->
+                <h2>Health Net</h2>
+                <!-- Project Role -->
+                <p class="proj-role">Development Coordinator</p>
+
+                <!-- Project Description -->
+                <p>
+                    Worked in a team of five to design, build, and deploy a health
+                    care web application. Utilized an incremental agile development cycle.
+                </p>
+
+                <!-- Related Tools -->
+                <h3>Tools</h3>
+                <ul>
+                    <li>Python 3</li>
+                    <li>HTML 5 // CSS 3 // JavaScript &amp; jQuery</li>
+                    <li>Django 1.9.1</li>
+                    <li>APIs: FullCalendar &amp; FusionChart</li>
+                    <li>Version Control: SVN / Tortoise SVN</li>
+                </ul>
+                <br>
+                <ul class="proj-links">
+                    <li><span class="ion ion-ios-email-outline"></span>Code Available Upon Request</li>
+                    <!-- TODO: Link to ReadMe File Page -->
+                    <!-- <li><span class="ion ion-ios-paper-outline"></span><a href="#">View ReadMe File</a></li> -->
+                </ul>
+            </div>
+
+            <!-- <div class="button-group">
+                <div>
+                    <span class="ion ion-ios-email-outline"></span> -->
+                    <!-- Opens a README File -->
+                    <!-- <a href="#" class="ion ion-ios-paper-outline"></a>
+                </div>
+
+            </div> -->
+
+        </div><!-- End of Project Block -->
+
+        <!-- Huffman Encoding -->
+        <div class="project-block" id="selected">
+
+            <div class="project-content">
+                <!-- Project Title -->
+                <h2>Huffman Encoding</h2>
+                <!-- Project Role -->
+                <p class="proj-role">Solo Developer</p>
+
+                <!-- Project Description -->
+                <p>
+                    Designed a file compression program utilizing a heap-based Huffman
+                    Encoding implementation. This program either outputs an encoded or
+                    decoded text file – based on the existence of a one byte 'magic'
+                    value in an inputted file.
+                </p>
+
+                <!-- Related Tools -->
+                <h3>Tools</h3>
+                <ul>
+                    <li>C Language (C-99 Standard)</li>
+                    <li>Valgrind</li>
+                    <li>GDB Debugger</li>
+                    <li>OS: Linux (accessed via puTTy)</li>
+                </ul>
+                <br>
+                <ul class="proj-links">
+                    <li><span class="ion ion-ios-email-outline"></span>Code Available Upon Request</li>
+                    <!-- TODO: Link to ReadMe File Page -->
+                    <!-- <li><span class="ion ion-ios-paper-outline"></span><a href="#">View ReadMe File</a></li> -->
+                </ul>
+            </div>
+
+            <!-- <div class="button-group">
+                <div>
+                    <span class="ion ion-ios-email-outline"></span> -->
+                    <!-- Opens a README File -->
+                    <!-- <a href="#" class="ion ion-ios-paper-outline"></a>
+                </div>
+
+            </div> -->
+
+        </div><!-- End of Project Block -->
+
+        <!-- Escape From Gringotts -->
+        <div class="project-block">
+
+            <div class="project-content">
+                <!-- Project Title -->
+                <h2>Escape From Gringotts</h2>
+                <!-- Project Role -->
+                <p class="proj-role">Co-Developer</p>
+
+                <!-- Project Description -->
+                <p>
+                    With a partner, developed a tile-based game that implemented
+                    a recursive backtracking algorithm to solve a given game
+                    configuration and to give the player hints.
+                </p>
+
+                <!-- Related Tools -->
+                <h3>Tools</h3>
+                <ul>
+                    <li>Java 8 &amp; JavaFX</li>
+                    <li>IntelliJ IDEA</li>
+                    <li>Version Control: Git</li>
+                </ul>
+                <br>
+                <ul class="proj-links">
+                    <li><span class="ion ion-ios-email-outline"></span>Code Available Upon Request</li>
+                    <!-- TODO: Link to ReadMe File Page -->
+                    <!-- <li><span class="ion ion-ios-paper-outline"></span><a href="#">View ReadMe File</a></li> -->
+                </ul>
+            </div>
+
+            <!-- <div class="button-group">
+                <div>
+                    <span class="ion ion-ios-email-outline"></span> -->
+                    <!-- Opens a README File -->
+                    <!-- <a href="#" class="ion ion-ios-paper-outline"></a>
+                </div>
+
+            </div> -->
+
+        </div><!-- End of Project Block -->
+
+    </div><!-- End of projects-container-inner -->
+
+    <!-- Right Arrow Button -->
+    <!-- <span class="arrows ion ion-android-arrow-dropright-circle" id="right_btn"></span> -->
+
+</div><!-- End of projects-container -->
+
+<!-- Icon Key for Project Cards -->
+<!-- <div id="key">
+    <h2>KEY</h2>
+    <ul>
+        <li><span class="ion ion-social-github-outline"></span> - Viewable on GitHub</li>
+        <li><span class="ion ion-ios-email-outline"></span> - Available Upon Request</li>
+        <li><span class="ion ion-ios-paper-outline"></span> - View README File</li>
+    </ul>
+</div> -->
